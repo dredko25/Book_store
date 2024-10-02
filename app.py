1f157d3e3bac973a4e12cde61a133a2a6e9ecf49
dredko25 <dredko25@gmail.com>
2024-10-02 13:44:48 +0300

    add connection to database

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 app = Flask(__name__)
 
+# Підключення до БД
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://@localhost/Book_store?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
+db = SQLAlchemy(app)
+
+
 @app.route('/')
 def main():
-    # Render the main page template.
-    return render_template('main.html')
+    try:
+        result = db.session.execute(text("SELECT * FROM Genre"))
+        genres = result.fetchall()
+
+        genres_list = [{column: value for column, value in zip(result.keys(), genre)} for genre in genres]
+
+        return render_template('main.html', genres=genres_list)
+
+    except Exception as e:
+        return f"Виникла помилка: {e}"
+    
     
 if __name__ == '__main__':
     app.run(debug=True)
+
 
