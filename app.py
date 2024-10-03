0e410c44b83ffca8f66c2221a07fcc2bcc2475a9
dredko25 <dredko25@gmail.com>
2024-10-03 12:45:55 +0300

    add links to genres

--- app.py
+++ app.py
@@ -27,6 +27,35 @@ def main():
     except Exception as e:
         return f"Виникла помилка: {e}"
     
+@app.route('/genre/<genre_name>')
+def genre_books(genre_name):
+    try:
+        # Отримати ID жанру за його назвою
+        genre_query = db.session.execute(text("SELECT ID_genre FROM Genre WHERE Name_genre = :genre_name"), {'genre_name': genre_name})
+        genre_result = genre_query.fetchone()
+        genre_id = genre_result[0]
+
+        # Отримати книги за ID жанру
+        b = db.session.execute(
+            text("SELECT B.ID_book, B.Book_name, A.A_Name, A.A_Patronymics, A.A_Surname, B.Price FROM Book as B JOIN Author as A ON B.ID_author = A.ID_author WHERE B.ID_genre = :genre_id"),
+            {'genre_id': genre_id}
+        )
+        books = b.fetchall()
+
+        books_list = [{column: value for column, value in zip(b.keys(), book)} for book in books]
+        
+        g = db.session.execute(text("SELECT * FROM Genre"))
+        genres = g.fetchall()
+
+        genres_list = [{column: value for column, value in zip(g.keys(), genre)} for genre in genres]
+
+        # Повернути шаблон з жанром та списком книг
+        return render_template('genre_books.html', genres=genres_list, genre=genre_name, books=books_list)
+
+    except Exception as e:
+        return f"Виникла помилка: {e}"
+
+    
     
 if __name__ == '__main__':
     app.run(debug=True)
