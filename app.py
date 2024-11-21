0506174ff2a9103ed350dc50275a3d95ee14a760
dredko25 <dredko25@gmail.com>
2024-11-21 14:37:16 +0200

    add routes

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'a8e9f8ad6cfd4e1bb5a34b7e8e2c9fd1afbd2c1f12a56d3e7f8a9e'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://@localhost/Book_store?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
 db = SQLAlchemy(app)
 
-def get_genres():
-    genres_query = db.session.execute(text("SELECT * FROM Genre"))
-    genres = genres_query.fetchall()
-    return [{column: value for column, value in zip(genres_query.keys(), genre)} for genre in genres]
+@app.context_processor
+def inject_genres():
+    genres_query = db.session.execute(text("SELECT Name_genre FROM Genre")).mappings().fetchall()
+    genres = [{'Name_genre': row['Name_genre']} for row in genres_query]
+    return {'genres': genres}
 
 @app.route('/')
 def main():
-    try:
-        genres_list = get_genres()
-        
+    try:        
         sort = request.args.get('sort', '')
         
         query = """
@@ -36,7 +37,7 @@ def main():
 
         books_list = [{column: value for column, value in zip(b.keys(), book)} for book in books]
 
-        return render_template('main.html', genres=genres_list, books=books_list)
+        return render_template('main.html', books=books_list)
 
     except Exception as e:
         return f"Виникла помилка: {e}"
@@ -70,10 +71,8 @@ def genre_books(genre_name):
         books = b.fetchall()
 
         books_list = [{column: value for column, value in zip(b.keys(), book)} for book in books]
-        
-        genres_list = get_genres()
 
-        return render_template('genre_books.html', genres=genres_list, genre=genre_name, books=books_list)
+        return render_template('genre_books.html', genre=genre_name, books=books_list)
 
     except Exception as e:
         return f"Виникла помилка: {e}"
@@ -81,9 +80,7 @@ def genre_books(genre_name):
     
 @app.route('/book/<int:book_id>')
 def book_details(book_id):
-    try:
-        genres_list = get_genres()
-        
+    try:        
         book_query = text("""
             SELECT B.ID_book, B.Book_name, A.A_Name, A.A_Patronymics, A.A_Surname, 
                    G.Name_genre AS Genre_Name, PH.Name_book AS Publishing_House,
@@ -120,11 +117,11 @@ def book_details(book_id):
             'Descriptions': book.Descriptions
         }
         
-        print(book_details)
+        # print(book_details)
         
         # reviews_list = [{'user_name': review.user_name, 'content': review.content} for review in reviews]
 
-        return render_template('book_page.html', book=book_details, genres=genres_list) #, reviews=reviews_list
+        return render_template('book_page.html', book=book_details) #, reviews=reviews_list
 
     except Exception as e:
         return f"Виникла помилка: {e}"
@@ -134,8 +131,6 @@ def search():
     try:
         query = request.args.get('query', '').strip()
         sort = request.args.get('sort', '')
-        
-        genres_list = get_genres()
 
         if query:
             b = db.session.execute(text("""
@@ -173,12 +168,92 @@ def search():
         else:
             message = None
 
-        return render_template('search_result.html', genres=genres_list, books=books_list, message=message, query=query)
+        return render_template('search_result.html', books=books_list, message=message, query=query)
 
     except Exception as e:
         return f"Виникла помилка: {e}"
 
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+
+    if not email or not password:
+        return "Заповніть усі поля!"
+
+    user_query = text("""
+        SELECT *
+        FROM Customer
+        WHERE user_login = :email
+    """)
     
+    user = db.session.execute(user_query, {'email': email}).fetchone()
+
+    if not user:
+        return "Користувача з таким email не знайдено!"
+
+    if not check_password_hash(user[2], password):
+        return "Неправильний пароль!"
+
+    session['user_login'] = email
+    session['user_name'] = user[4]
+    session['user_surname'] = user[3]
+    session['user_patronymics'] = user[5]
+    session['user_phone'] = user[6]
+    session['user_address'] = user[7]
+
+    return redirect(url_for('main'))
+
+@app.route('/register', methods=['POST'])
+def register():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    middle_name = request.form.get('middle_name', None)
+    phone = request.form['phone']
+    address = request.form['delivery_address']
+    email = request.form['email']
+    password = request.form['password']
+
+    if not first_name or not last_name or not phone or not address or not email or not password:
+        return "Усі поля обов'язкові для заповнення!"
+
+    hashed_password = generate_password_hash(password)
+
+    customer_query = text("""
+        INSERT INTO Customer (user_login, user_password, C_Surname, C_Name, C_Patronymics, Phone_number, Addres)
+        VALUES (:email, :password, :last_name, :first_name, :middle_name, :phone, :address)
+    """)
+
+    try:
+        db.session.execute(customer_query, {
+            'email': email,
+            'password': hashed_password,
+            'first_name': first_name,
+            'last_name': last_name,
+            'middle_name': middle_name,
+            'phone': phone,
+            'address': address
+        })
+        db.session.commit()
+        
+        session['user_login'] = email
+        session['user_name'] = first_name
+        session['user_surname'] = last_name
+        session['user_patronymics'] = middle_name
+        session['user_phone'] = phone
+        session['user_address'] = address
+        
+        return redirect(url_for('main'))
+    except Exception as e:
+        db.session.rollback()
+        return f"Сталася помилка: {e}"
+
+@app.route('/logout')
+def logout():
+    session.clear()
+
+    return redirect(url_for('main'))
+  
     
 if __name__ == '__main__':
     app.run(debug=True)
